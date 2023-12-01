ef3953142050477b405f5d1e9f5d0b059d9b4dbf
rreyes5132 <rreyes5132@ceuandalucia.es>
2023-12-01 20:01:23 +0100

    modificada clase

--- src/entornos/Turron.java
+++ src/entornos/Turron.java
@@ -5,6 +5,7 @@ public class Turron {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Me llamo Rocío y me encanta el turrón");
+		System.out.println("Pero mi preferido es el de chocolate.");
 	}
 
 }
